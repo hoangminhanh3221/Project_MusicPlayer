b70ed06d41a2e2b08868b8d614fdcdd16c4998e7
hoangminhanh3221 <hoangminhanh3221@gmail.com>
2023-09-03 16:02:32 +0700

    Create AccountAPI.java

---
+++ Spotify_BE/src/main/java/com/spotify/controller/AccountAPI.java
@@ -0,0 +1,64 @@
+package com.spotify.controller;
+
+import com.spotify.entity.Account;
+import com.spotify.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/account")
+public class AccountAPI {
+
+    private final AccountService accountService;
+
+    @Autowired
+    public AccountAPI(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Account>> getAllAccount() {
+        return ResponseEntity.ok(accountService.getAllAccount());
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<HttpStatus> createAccount(@RequestBody Account account){
+        if(account != null){
+            accountService.createAccount(account);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping ("/update")
+    public ResponseEntity<HttpStatus> updateAccount(
+            @RequestBody Account account
+    ){
+        Optional<Account> currentAccount = accountService.getAccountById(account.getAccountId());
+        if(currentAccount.isPresent()){
+            accountService.updateAccount(account);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping ("/delete/{id}")
+    public ResponseEntity<HttpStatus> deleteAccount(
+            @PathVariable("id") String id
+    ){
+        Optional<Account> currentAccount = accountService.getAccountById(id);
+        if(currentAccount.isPresent()){
+            accountService.deleteAccount(id);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
